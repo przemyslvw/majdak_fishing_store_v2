1cc49de0bb6194a03338e3187edc6b90b8e67450
przemyslvw <przemyslvw@gmial.com>
2024-06-12 12:27:47 +0200

    header and bootstrap

--- functions.php
+++ functions.php
@@ -8,3 +8,12 @@
  * @subpackage majdak_online_store_v2
  * @since 1.0.0
  */
+function majdakonline_enqueue_bootstrap()
+{
+    error_log('Enqueue Bootstrap: Start');
+    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css', array(), '5.3.3', 'all');
+    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', array('jquery'), '5.3.3', true);
+    error_log('Enqueue Bootstrap: Done');
+}
+
+add_action('wp_enqueue_scripts', 'majdakonline_enqueue_bootstrap');
---
+++ patterns/header.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Title: Header with colophon, 4 columns
+ * Slug: majdakonline/header
+ * Categories: header
+ * Block Types: core/template-part/header
+ */
+?>
+
+<div>
+    header
+</div>
