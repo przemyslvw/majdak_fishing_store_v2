95d02d41dbc3fca5bd4874f1c189ede44419e847
przemyslvw <przemyslvw@gmial.com>
2024-06-12 12:28:05 +0200

    produstpage on frontpage

--- patterns/posts-list.php
+++ patterns/posts-list.php
@@ -7,13 +7,58 @@
  * Block Types: core/query
  */
 ?>
+<?php
+// Upewnij się, że WooCommerce jest aktywny
+if (class_exists('WooCommerce')) : ?>
 
+    <div class="container">
+        <div class="row">
+            <?php
+            // Argumenty dla zapytania o produkty
+            $args = array(
+                'post_type' => 'product',
+                'posts_per_page' => 16, // Wyświetl 16 produktów
+                'paged' => get_query_var('paged') ? get_query_var('paged') : 1 // Paginacja
+            );
 
-<!-- wp:post-template -->
-<!-- wp:separator {"backgroundColor":"contrast-3","className":"alignwide is-style-wide"} -->
-<hr class="wp-block-separator has-text-color has-contrast-3-color has-alpha-channel-opacity has-contrast-3-background-color has-background alignwide is-style-wide" />
-<!-- /wp:separator -->
+            // Zapytanie o produkty
+            $loop = new WP_Query($args);
 
+            if ($loop->have_posts()) : ?>
+                <?php while ($loop->have_posts()) : $loop->the_post();
+                    global $product; ?>
+                    <div class="col-md-3 product">
+                        <!-- Obrazek produktu w formacie kwadratowym -->
+                        <div class="product-image" style="width:100%; padding-top:100%; background-size:cover; background-position:center; background-image:url('<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>');">
+                        </div>
+                        <!-- Tytuł produktu jako link, z odpowiednim stylem -->
+                        <h2 class="product-title" style="line-height: 1.1; font-size: 1.5rem;">
+                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                        </h2>
 
-<!-- wp:post-title {"isLink":true,"style":{"typography":{"lineHeight":"1.1","fontSize":"1.5rem"}}} /-->
-<!-- wp:template-part {"slug":"post-meta"} /-->
+                        <!-- Cena produktu -->
+                        <span class="price"><?php echo $product->get_price_html(); ?></span>
+
+                        <!-- Przycisk Dodaj do koszyka -->
+                        <?php woocommerce_template_loop_add_to_cart(); ?>
+                    </div>
+                <?php endwhile; ?>
+                <!-- Paginacja -->
+                <div class="col-12">
+                    <?php
+                    echo paginate_links(array(
+                        'total' => $loop->max_num_pages,
+                        'prev_text' => __('« Poprzednia'),
+                        'next_text' => __('Następna »'),
+                    ));
+                    ?>
+                </div>
+            <?php else : ?>
+                <p><?php _e('Nie znaleziono produktów.', 'textdomain'); ?></p>
+            <?php endif; ?>
+
+            <?php wp_reset_postdata(); ?>
+        </div>
+    </div>
+
+<?php endif; ?>
